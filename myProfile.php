26c16f625d8d32a1e22736768e9d2c68c8fbac19
nitigan.n <nitigan.n@fitm.kmutnb.ac.th>
2020-12-24 15:13:22 +0700

    update

--- myProfile.php
+++ myProfile.php
@@ -1,4 +1,9 @@
 <?php
-    echo "Hello";
-    echo "Hello Nitigan";
+    <?php
+	define("x", 10);
+	echo "The value of the constant is x. <br />";
+	echo "The value of the constant is <b>";
+	echo x;
+	echo "</b>";
+?>
 ?>
